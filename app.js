c5ce854b621b8537d3dff7336a125e5d7155a7e6
Constanza Pérez <constanza.vp.o@gmail.com>
2025-01-27 20:12:12 -0300

    onclick en btn intentar

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Juego del número secreto'
 
 const parrafo = document.getElementById('parrafo')
 parrafo.innerHTML = 'Ingresa un número del 1 al 10'
+
+const intentar = () => {
+  alert('Alert desde el btn')
+}
