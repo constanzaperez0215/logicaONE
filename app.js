693e6d2570308b3ab38832e073fa68c397f309f6
Constanza Pérez <constanza.vp.o@gmail.com>
2025-01-27 20:08:09 -0300

    titulo y parrafo

---
+++ app.js
@@ -0,0 +1,5 @@
+const titulo = document.querySelector('h1')
+titulo.innerHTML = 'Juego del número secreto'
+
+const parrafo = document.getElementById('parrafo')
+parrafo.innerHTML = 'Ingresa un número del 1 al 10'
