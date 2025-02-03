bc721a4da70225908fb86cb1fe98e35f58dd054d
Constanza Pérez <constanza.vp.o@gmail.com>
2025-02-03 20:35:18 -0300

    Juego del número secreto

--- app.js
+++ app.js
@@ -1,9 +1,60 @@
-const titulo = document.querySelector('h1')
-titulo.innerHTML = 'Juego del número secreto'
+let numeroSecreto
+let intentos
 
-const parrafo = document.getElementById('parrafo')
-parrafo.innerHTML = 'Ingresa un número del 1 al 10'
+function condicionesIniciales () {
+  // Textos incertados en el html
+  insertarTextoElemento('h1', 'Juego del número secreto!!')
+  insertarTextoElemento('p', 'Ingresa un número del 1 al 10')
+  numeroSecreto = generarElNumeroSecreto()
+  intentos = 1
+  console.log(numeroSecreto)
+}
+condicionesIniciales()
 
-const intentar = () => {
-  alert('Alert desde el btn')
+
+// Inserta textos en el html
+function insertarTextoElemento (elemento, texto) {
+  const elementoHTML = document.querySelector(elemento)
+  elementoHTML.innerHTML = texto
+}
+
+
+// Generador de números random
+function generarElNumeroSecreto () {
+  return Math.floor(Math.random()*10)
+}
+
+
+function verificarIntento () {
+  // Recupera el valor del input y verifica que el valor ingresado sea un número
+  const numeroUsuario = parseInt(document.getElementById('numeroUsuario').value)
+
+// Verifica si el número secreto con el número del usuario son iguales
+  if (numeroUsuario === numeroSecreto) {
+    insertarTextoElemento('p',`Acertaste el número secreto en ${intentos} ${(intentos == 1) ? 'vez' : 'veces'}`)
+    document.querySelector('#reiniciar').removeAttribute('disabled')
+  } else {
+    (numeroUsuario > numeroSecreto) ? insertarTextoElemento('p','El número es menor') : insertarTextoElemento('p','el numero secreto es mayor')
+  }
+
+  intentos++
+  vaciarInput()
+}
+
+
+function vaciarInput () {
+  let inputEnBlanco = document.querySelector('#numeroUsuario')
+  inputEnBlanco.value = ''
+}
+
+
+function reuniciarJuego () {
+  // limpiar caja
+  vaciarInput()
+
+  // reinicia todas las conficiones iniciales
+  condicionesIniciales()
+
+  // deshabilitar el btn reiniciar
+  document.querySelector('#reiniciar').setAttribute('disabled', 'true')
 }
